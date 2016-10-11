77ea7a2873c2703dc3c1f0e71457190ca1c90a51
vcampmany <vcampmany@gmail.com>
2016-10-11 23:28:46 +0200

    added cmakelists

---
+++ build/CMakeFiles/visionSys.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/victor/CudaVisionSysDeploy/src/common/AccumulativeRefinement.cpp" "/home/victor/CudaVisionSysDeploy/build/CMakeFiles/visionSys.dir/src/common/AccumulativeRefinement.o"
+  "/home/victor/CudaVisionSysDeploy/src/common/Maths.cpp" "/home/victor/CudaVisionSysDeploy/build/CMakeFiles/visionSys.dir/src/common/Maths.o"
+  "/home/victor/CudaVisionSysDeploy/src/common/Refinement.cpp" "/home/victor/CudaVisionSysDeploy/build/CMakeFiles/visionSys.dir/src/common/Refinement.o"
+  "/home/victor/CudaVisionSysDeploy/src/common/RoiCluster.cpp" "/home/victor/CudaVisionSysDeploy/build/CMakeFiles/visionSys.dir/src/common/RoiCluster.o"
+  "/home/victor/CudaVisionSysDeploy/src/init/fileInOut/IniFileIO.cpp" "/home/victor/CudaVisionSysDeploy/build/CMakeFiles/visionSys.dir/src/init/fileInOut/IniFileIO.o"
+  "/home/victor/CudaVisionSysDeploy/src/init/fileInOut/Utils.cpp" "/home/victor/CudaVisionSysDeploy/build/CMakeFiles/visionSys.dir/src/init/fileInOut/Utils.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/cuda/targets/aarch64-linux/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
